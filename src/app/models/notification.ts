a4f7dc8f194bc722220ee4a491e6db5ca8c661d4
vpellegrino <pellegrino.valentino@gmail.com>
2018-07-25 13:32:50 +0200

    Add models

---
+++ src/app/models/notification.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by vpellegr on 20/07/2018.
+ */
+
+export class Notification {
+    id: string;
+    title: string;
+    category: string;
+    description: string;
+    created_at: string;
+}
---
+++ src/app/models/service.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by vpellegr on 20/07/2018.
+ */
+
+export class Service {
+    name: string;
+    version: number;
+    update_at: string;
+}
